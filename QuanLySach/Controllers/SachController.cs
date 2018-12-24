e51e9f9a61fda4489bfa2da47fb61fb17764fcfd
phucs <phuc.ha@bookoke.com>
2018-12-24 19:51:35 +0700

    khoi tao du an

---
+++ QuanLySach/Controllers/SachController.cs
@@ -0,0 +1,34 @@
+﻿using QuanLySach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuanLySach.Controllers
+{
+    public class SachController : ApiController
+    {
+        Sach[] sachs = new Sach[]
+        {
+            new Sach {id = 1, title = "Tôi thấy hoa vàng trên cỏ xanh",authorName = "Nguyễn Nhật Ánh", price=1, content = "Truyện kể về Tuổi thơ"}   ,
+            new Sach {id = 2, title = "Pro ASP.NET MVC5",  authorName = "Adam Freeman",price= 3.75M, content =  "The ASP.NET MVC5 Framework is the lastest evolution of Microsoft's ASP.NET web platform."}
+        };
+
+        public IEnumerable<Sach> GetAll()
+        {
+            return sachs;
+        }
+
+        public IHttpActionResult GetSach(int id)
+        {
+            var sach = sachs.FirstOrDefault((p) => p.id == id);
+            if(sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+    }
+}
---
+++ QuanLySach/Models/Sach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLySach.Models
+{
+    public class Sach
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public string authorName { get; set; }
+        public decimal price { get; set; }
+    }
+
+}
